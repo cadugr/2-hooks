8f1db923fe2892a33beb5ab0f607fc92b20ed2f4
Cadu Guerra <cadu.guerra@gmail.com>
2022-08-07 19:42:52 -0300

    Finalizando a aula 3.8 sobre gerenciamento de estado complexo com useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,59 @@
-import { useEffect, useRef } from 'react';
+import { useReducer } from 'react';
 import './App.css';
-import Button from './Button';
+
+interface InitialState {
+  count: number
+}
+
+type Action = 
+{type: 'DECREMENT'} |
+{type: 'INCREMENT', payload: number}
+
+const initialState: InitialState = {
+  count: 1
+}
+/**Essa função reducer, obrigatoriamente recebe o estado e uma ação.  Obrigatoriamente ela precisa 
+ * retornar um estado.
+ */
+function reducer(state: InitialState, action: Action): InitialState {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.payload
+      }
+      case 'DECREMENT':
+        return {
+          count: state.count - 1
+        }  
+      default:
+        return state  
+  } 
+}
 
 function App() {
-  const button = useRef<HTMLButtonElement>(null)
 
-  /**Em componentes funcionais não podemos passar uma ref como propriedade e sim devemos usar o fowardRef */
+  /**O useReducer, espera dois parametros: uma função e o estado inicial*/
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-  useEffect(() => {
-    setTimeout(() => {
-      button.current?.click()
-    }, 2000)
-  }, [])
-
+  /**A idéia para utilizarmos o hook useReducer é para gerenciarmos estados locais de forma mais complexa.
+   * Funciona de forma muito parecida com o redux.  O método dispatch, faz com que a função reducer seja executada.
+   * O ideal para utilizar um reducer é quando temos muita lógica para gerenciar o nosso estado.
+   */
   return (
     <div className="App">
-      <Button ref={button} title='clique em mim'/>
+      <div style={{backgroundColor: 'peachpuff'}}>
+        { state.count }
+      </div>
+      <button onClick={() => {
+        dispatch({ type: 'INCREMENT', payload: 2 })
+      }}>
+        acrescer
+      </button>
+      <button onClick={() => {
+        dispatch({ type: 'DECREMENT' })
+      }}>
+        diminuir
+      </button>
     </div>
   );
 }
