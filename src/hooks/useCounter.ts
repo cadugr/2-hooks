bf1e65ab94cfefabe52bf00166de204722326cae
Cadu Guerra <cadu.guerra@gmail.com>
2022-08-07 11:21:21 -0300

    Finalizando a aula 3.5 sobre como passar parametros para os hooks

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
 
-export default function useCounter() {
+export default function useCounter(initialNumber = 1) {
 
     /**Podemos criar estados dentro de hooks.  Porém, é importante lembrar que não podemos
      * gerar efeito colateral na raiz do nosso componente.  Podemos tranquilamente utilizar um
      * hook, dentro de outro
      */
 
-    const [number, setNumber] = useState(1)
+    const [number, setNumber] = useState(initialNumber)
 
     /**Não podemos utilizar então, métodos como este setTimeout dentro de um hook, pois gera efeito colateral.
      * Neste caso, podemos usar o useEffect.
@@ -18,9 +18,9 @@ export default function useCounter() {
 
     useEffect(() => {
         setTimeout(() => {
-            setNumber(2)
+            setNumber(number + 1)
         }, 5000)
-    }, [])
+    }, [ ])
 
     return number
 
